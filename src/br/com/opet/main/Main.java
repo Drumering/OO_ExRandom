3e95c3b3f7b57f759c2460d631995aa1bc2e0ffb
opet <opet@RB-LAB207A-003.Academico.edu>
2019-03-06 20:26:19 -0300

    update iteracoes hashmap

--- src/br/com/opet/main/Main.java
+++ src/br/com/opet/main/Main.java
@@ -1,9 +1,36 @@
 package br.com.opet.main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+
+import com.opet.util.Reader;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+
+		Random rdn = new Random();
+
+		HashMap Lista = new HashMap();
+		int contagemIteracoes = 0;
+		while (Lista.size() < 100) {
+			Lista.put(rdn.nextInt(200),contagemIteracoes);
+			contagemIteracoes++;
+		}
+		
+		System.out.println("Informe um número de 0 a 199");
+		int leia = Reader.readInt();
+		
+		if(Lista.containsValue(leia)) {
+			System.out.println("Possui o numero informado");
+			System.out.println("Houveram " + Lista.get(leia) + " itaracoes para obte-lo");
+		}else if(leia > 200){
+			System.out.println("Informe um numero dentro do Range de 0 a 200");
+		}else {
+			System.out.println("Nao possui o numero informado");
+		}
 
 	}
 
